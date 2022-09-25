c9577d8d597a2391d69d57a925c2fd71ff7300ef
Rafael Cena <rafael.cena@hotmail.com>
2022-09-25 08:35:43 -0300

    create deliverie

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import { AuthenticateClientController } from "./modules/account/authenticateClie
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController"
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController"
 import { CreateDeliveryController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliveryController"
-
+import { DeliveryController } from "./modules/deliveries/useCases/createDelivery/createDeliveryController"
 
 const routes = Router()
 
@@ -11,15 +11,17 @@ const createClientController = new CreateClientController()
 const authenticateClientController = new AuthenticateClientController()
 const createDeliveryController = new CreateDeliveryController()
 const authenticateDeliverymanController = new AuthenticateDeliverymanController()
-
+const deliveryController = new DeliveryController()
 
 routes.post("/client/authenticate" , authenticateClientController.handle)
-routes.post("/deliveryman/authenticate" , authenticateClientController.handle)
+routes.post("/deliveryman/authenticate" , authenticateDeliverymanController.handle)
 
 
 routes.post("/client/", createClientController.handle )
 
 routes.post("/deliveryman/", createDeliveryController.handle )
+
+routes.post("/deliverie/", deliveryController.handle )
 
 
 export { routes }
